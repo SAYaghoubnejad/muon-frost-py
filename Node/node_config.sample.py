bc14fa60564188498a87ed67d896a3aa0f00df40
SAYaghoubnejad <s.ali.yaghoubnejad@gmail.com>
2023-11-05 12:53:35 +0330

    Add configs sample file

---
+++ Node/node_config.sample.py
@@ -0,0 +1,8 @@
+PRIVATES = {
+    '16Uiu2HAm7Sx71kCEvgK8drUWZACPhU2WiUftZPSKjbAC5accWqwE': 
+        '',
+    '16Uiu2HAmBep4CggnrJX36oQ1S5z8T9VTrjXS66Tskx2QzQJonkr2': 
+        '',
+    '16Uiu2HAmUSf3PjDQ6Y1eBPU3TbDFXQzsf9jmj4qyc7wXMGKceo2K': 
+        '',
+}
