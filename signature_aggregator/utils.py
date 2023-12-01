f12987334172cfd04959a8c167319c9e18b39f66
m.chitgarha78 <m.chitgarha78@gmail.com>
2023-12-01 15:34:08 +0330

    Minor fixes

--- signature_aggregator/utils.py
+++ signature_aggregator/utils.py
@@ -7,6 +7,8 @@ from typing import Dict, List
 import trio
 import types
 import json
+import logging
+
 class RequestObject:
     def __init__(self, request_id: str, call_method: str, gateway_authorization: str, parameters: Dict) -> None:
         self.request_id: str = request_id
@@ -28,24 +30,25 @@ class RequestObject:
 
 class Wrappers:
     @staticmethod
-    async def sign(function: types.FunctionType, dkg_key, destination_address: Dict[str, str], destination_peer_id: PeerID, protocol_id: TProtocol,
+    async def sign(send: types.FunctionType, dkg_key, destination_address: Dict[str, str], destination_peer_id: PeerID, protocol_id: TProtocol,
                    message: Dict, result: Dict = None, timeout: float = 5.0, semaphore: trio.Semaphore = None):
-        await function(destination_address, destination_peer_id, protocol_id,
+        
+        await send(destination_address, destination_peer_id, protocol_id,
                                       message, result,timeout, semaphore)
-        if protocol_id == PROTOCOLS_ID['sign'] and result is not None and\
-                            result.get(destination_peer_id) is not None and \
-                            result[destination_peer_id]['status'] == 'SUCCESSFUL':
-                
-                sign = result[destination_peer_id]['signature_data']
-                msg = result[destination_peer_id]['data']
-                encoded_message = json.dumps(msg)
-                commitments_dict = message['parameters']['commitments_list']
-                aggregated_public_nonce = TSS.code_to_pub(sign['aggregated_public_nonce'])
-                
-                res = TSS.frost_verify_single_signature(sign['id'], encoded_message, 
-                                                        commitments_dict,
-                                                        aggregated_public_nonce, 
-                                                        dkg_key['public_shares'][sign['id']], 
-                                                        sign, dkg_key['public_key'])
-                if not res:
-                    result[destination_peer_id]['status'] = 'MALICIOUS'
+        
+        if result[destination_peer_id]['status'] != 'SUCCESSFUL':
+            return
+        
+        sign = result[destination_peer_id]['signature_data']
+        msg = result[destination_peer_id]['data']
+        encoded_message = json.dumps(msg)
+        commitments_dict = message['parameters']['commitments_list']
+        aggregated_public_nonce = TSS.code_to_pub(sign['aggregated_public_nonce'])
+        
+        res = TSS.frost_verify_single_signature(sign['id'], encoded_message, 
+                                                commitments_dict,
+                                                aggregated_public_nonce, 
+                                                dkg_key['public_shares'][sign['id']], 
+                                                sign, dkg_key['public_key'])
+        if not res:
+            result[destination_peer_id]['status'] = 'MALICIOUS'
